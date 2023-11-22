f41b061ce6166d0fe576f9dde20f465e1b225b03
Ali Olayan <aliolayan12@gmail.com>
2023-11-22 13:21:40 +0100

    third commit

---
+++ analogClock.js
@@ -0,0 +1,22 @@
+function updateClock() {
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+    const seconds = currentTime.getSeconds();
+  
+    const hourHand = document.querySelector('.hour-hand');
+    const minHand = document.querySelector('.min-hand');
+    const secondHand = document.querySelector('.second-hand');
+  
+    const hourAngle = (hours % 12) * 30 + minutes * 0.5;
+    const minAngle = minutes * 6;
+    const secondAngle = seconds * 6;
+  
+    secondHand.style.transform = `rotate(${secondAngle}deg)`;
+    hourHand.style.transform = `rotate(${hourAngle}deg)`;
+    minHand.style.transform = `rotate(${minAngle}deg)`;
+    
+  }
+  
+  setInterval(updateClock, 1000); // Update every second
+  updateClock(); // Initial update
